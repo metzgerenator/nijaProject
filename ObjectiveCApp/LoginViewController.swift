0c5c7ef6c5f6d666ba72e0884bd63985b2de8526
Michael Metzger <metzgerenator@gmail.com>
2016-10-13 08:00:53 -0400

    added login and listener

---
+++ ObjectiveCApp/LoginViewController.swift
@@ -0,0 +1,80 @@
+//
+//  LoginViewController.swift
+//  ObjectiveCApp
+//
+//  Created by Aileen Taboy on 10/13/16.
+//  Copyright © 2016 Michael Metzger . All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var passWord: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        FIRAuth.auth()?.addStateDidChangeListener { auth, user in
+            if let user = user {
+                // User is signed in.
+                
+                self.performSegue(withIdentifier: "success", sender: nil)
+            } else {
+                // No user is signed in.
+            }
+        }
+        
+       
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func loginButton(_ sender: AnyObject) {
+        
+        
+        
+            
+            FIRAuth.auth()?.signIn(withEmail: userName.text!, password: passWord.text!, completion: { (user, error) in
+                
+                print("user \(user) error \(error)")
+                
+                
+            })
+            
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
