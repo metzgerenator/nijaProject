4aecee8c6d54f0ebded550c30f89e1dfb4bda9d6
Michael Metzger <metzgerenator@gmail.com>
2017-01-17 20:40:18 -0500

    added mmap search

--- ObjectiveCApp/SelectLocationViewController.swift
+++ ObjectiveCApp/SelectLocationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class SelectLocationViewController: UIViewController {
 
@@ -17,7 +18,10 @@ class SelectLocationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         searchBar.delegate = self
+        self.searchBar.endEditing(true)
         // Do any additional setup after loading the view.
     }
 
@@ -46,8 +50,31 @@ class SelectLocationViewController: UIViewController {
 
 extension SelectLocationViewController: UISearchBarDelegate {
     
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+    }
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print("here is searchText \(searchText)")
+        
+        let localSearhRequest = MKLocalSearchRequest()
+        localSearhRequest.naturalLanguageQuery = searchBar.text
+        
+       let localSearch = MKLocalSearch(request: localSearhRequest)
+        
+        if localSearch.isSearching {
+            localSearch.cancel()
+        } else {
+            
+            localSearch.start(completionHandler: { (response, error) in
+                let results = response?.mapItems
+                print("here are the results \(results)")
+            })
+        }
+        
+        
+        
     }
     
     
