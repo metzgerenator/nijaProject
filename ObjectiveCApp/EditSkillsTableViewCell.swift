06b4252ad076df80a14a89a3a22a0533d74fae2d
metzgerenator <metzgerenator@gmail.com>
2017-01-21 10:59:55 -0500

    setup skills tableview

---
+++ ObjectiveCApp/EditSkillsTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  EditSkillsTableViewCell.swift
+//  ObjectiveCApp
+//
+//  Created by Michael Metzger  on 1/21/17.
+//  Copyright © 2017 Michael Metzger . All rights reserved.
+//
+
+import UIKit
+
+class EditSkillsTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var skillButton: UILabel!
+    
+    @IBAction func additionalSkillsButton(_ sender: UIButton) {
+        
+        
+        
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ObjectiveCApp/skillsStruct.swift
@@ -0,0 +1,19 @@
+//
+//  skillsStruct.swift
+//  ObjectiveCApp
+//
+//  Created by Michael Metzger  on 1/21/17.
+//  Copyright © 2017 Michael Metzger . All rights reserved.
+//
+
+import Foundation
+
+struct DeveloperSkills {
+    
+    var skillType: String?
+    var subSkills: [String]?
+    
+  //pull in additional skills with firebase
+    
+}
+
